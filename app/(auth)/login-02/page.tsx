bb90a593e380986ab6d091287636959189a8429c
Alex Voievudko <oleksandrv@zeptolab.com>
2023-10-05 12:47:33 +0200

    feat: added mobile screen warnings

--- app/(auth)/login-02/page.tsx
+++ app/(auth)/login-02/page.tsx
@@ -9,23 +9,30 @@ export const metadata: Metadata = {
 
 const Login01Page = () => {
   return (
-    <main className='w-full h-screen grid grid-cols-2'>
-      <div className='flex flex-col p-12'>
-        <Image src='/logo.svg' alt='logo' width={32} height={32} priority />
-        <div className='flex-1 flex justify-center items-center'>
-          <LoginForm />
+    <>
+      <main className='flex items-center justify-center h-screen xl:hidden px-12'>
+        <h1 className='heading-xs text-center'>
+          Please view this page on desktop to see the full experience. Thank you! 🙏
+        </h1>
+      </main>
+      <main className='w-full h-screen xl:grid grid-cols-2 hidden'>
+        <div className='flex flex-col p-12'>
+          <Image src='/logo.svg' alt='logo' width={32} height={32} priority />
+          <div className='flex-1 flex justify-center items-center'>
+            <LoginForm />
+          </div>
         </div>
-      </div>
-      <div className='relative'>
-        <Image
-          src={login02}
-          alt='login-01'
-          fill
-          priority
-          className='object-cover rounded-tl-[24px] rounded-bl-[24px]'
-        />
-      </div>
-    </main>
+        <div className='relative'>
+          <Image
+            src={login02}
+            alt='login-01'
+            fill
+            priority
+            className='object-cover rounded-tl-[24px] rounded-bl-[24px]'
+          />
+        </div>
+      </main>
+    </>
   );
 };
 
